546a3ae591692e9b039811cbda23053c8262dc4a
EdEquinox <zemarquez97@hotmail.com>
2024-05-16 13:35:19 +0100

    chore: Update bundle directory path for assets in __init__.py

--- musicboardextract_gui/__init__.py
+++ musicboardextract_gui/__init__.py
@@ -13,13 +13,20 @@ from .modules.extract_data import extract_list_album
 from .modules.extract_data import extract_list_later
 from .modules.extract_data import extract_list_reviews
 import os
+import sys
 
 # Define any global variables or constants here
 
 APP_GEOMETRY = "800x600"
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
-BACKGROUND_IMG = os.path.join(CURRENT_DIR, "assets", "Sprinkle.png")
-ICON_PATH = os.path.join(CURRENT_DIR, "assets", "logo_musicboard.ico")
+
+if getattr(sys, 'frozen', False):
+    bundle_dir = sys._MEIPASS
+else:
+    bundle_dir = os.path.dirname(os.path.abspath(__file__))
+
+BACKGROUND_IMG = os.path.join(bundle_dir, "assets", "Sprinkle.png")
+ICON_PATH = os.path.join(bundle_dir, "assets", "logo_musicboard.ico")
 
 # Define any functions or classes here
 
