eec6906a3d977b11f7cb319786979ead195f30a8
EdEquinox <zemarquez97@hotmail.com>
2024-05-17 12:54:35 +0100

    chore: Add delay before running the GUI

--- musicboardextract.py
+++ musicboardextract.py
@@ -1,10 +1,12 @@
 
 from musicboardextract_gui import setup_app, setup_buttons
 import traceback
+import time
 
 try:
     app = setup_app()
     buttons = setup_buttons(app)
+    time.sleep(10)
     app.mainloop()
 except Exception:
     traceback.print_exc()
